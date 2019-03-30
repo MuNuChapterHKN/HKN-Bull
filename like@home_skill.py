1a78e3d7f567b8fc5d84d0361bb74a659ab82d74
Gialbo <gilbertomanunza@gmail.com>
2019-03-30 15:53:21 +0100

    bug finxing on api version

--- like@home_skill.py
+++ like@home_skill.py
@@ -3,25 +3,23 @@ This skills provides a quick description about the event
 Like@Home organized by the Mu Nu Chapter of IEEE-Eta Kappa Nu
 """
 import aiy.assistant.grpc
-import aiy.audio
-import aiy.voicehat
+import aiy.voice.audio
+from aiy.board import Board, Led
+from aiy.cloudspeech import CloudSpeechClient
+import aiy.voice.tts
+
 import sys
 
-aiy.i18n.set_language_code('it-IT')
 
 def main():
-    status_ui = aiy.voicehat.get_status_ui()
-    status_ui.status('starting')
-    assistant = aiy.assistant.grpc.get_assistant()
-    button = aiy.voicehat.get_button()
-    with aiy.audio.get_recorder():
+    with Board() as board:
+        hints = get_hints(args.language)
+        client = CloudSpeechClient()
         while True:
-            status_ui.status('ready')
             print('Press button to send commands to the robot')
-            button.wait_for_press()
-            status_ui.status('listening')
-            print('Listening...')
-            text, audio = assistant.recognize()
+            board.wait_for_press()
+            text = client.recognize(language_code=args.language,
+                                    hint_phrases=hints)
             if text:
                 print("You said : " + text)
                 if text == "Qual è l'evento migliore del mondo?":
@@ -30,10 +28,10 @@ def main():
                             L'evento si terrà sabato 30 marzo e domenica 31. Nel costo del biglietto sono inclusi l'ingresso e parecchi gadget. Il pranzo, la merenda, la cena, la colazione,
                             caffe e redbull sono gratuiti per tutti i partecipanti.
                             Cosa aspettate? Sentitevi Like at Home!"""
-                    aiy.audio.say(text)
+                    aiy.voice.tts.say(text)
                 else:
                     text = "ok google qual e l'evento migliore del mondo?"
-                    aiy.audio.say(text)
+                    aiy.voice.tts.say(text)
 
 if __name__ == "__main__":
     main()
